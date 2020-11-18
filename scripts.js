acdf211c51bf1746d07d4f8e7d0c24e0331bea8c
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-11-17 22:52:55 -0300

    test

--- scripts.js
+++ scripts.js
@@ -5,38 +5,38 @@ const card = document.querySelector("div.card");
 
 const videos = [
   {
-    title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+    title: "Formulário Animado com JS puro e CSS Animation | Kaik Oliveira",
     duration: "57 min",
     thumb: "https://i.ytimg.com/vi/GykTLqODQuU/hqdefault.jpg",
     video_id: "GykTLqODQuU"
   },
   {
-    title: "Desvendando o CSS Grid na prática | Mayk Brito",
+    title: "Desvendando o CSS Grid na prática | Kaik Oliveira",
     duration: "36 min",
     thumb: "https://i.ytimg.com/vi/HN1UjzRSdBk/hqdefault.jpg",
     video_id: "HN1UjzRSdBk"
   },
   {
-    title: "Array: Higher Order Functions | Mayk Brito",
+    title: "Array: Higher Order Functions | Kaik Oliveira",
     duration: "54 min",
     thumb: "https://i.ytimg.com/vi/rAzHvYnQ8DY/hqdefault.jpg",
     video_id: "rAzHvYnQ8DY"
   },
   {
-    title: "O que é API? REST e RESTful? | Mayk Brito",
+    title: "O que é API? REST e RESTful? | Kaik Oliveira",
     duration: "33 min",
     thumb: "https://i.ytimg.com/vi/ghTrp1x_1As/hqdefault.jpg",
     video_id: "ghTrp1x_1As"
   },
   {
-    title: "Desvendando a variável this no Javascript | Mayk Brito",
+    title: "Desvendando a variável this no Javascript | Kaik Oliveira",
     duration: "48 min",
     thumb: "https://i.ytimg.com/vi/GSqR2i-Pq6o/hqdefault.jpg",
     video_id: "GSqR2i-Pq6o"
   },
   {
     title:
-      "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+      "Como usar Git e Github na prática: Guia para iniciantes | Kaik Oliveira",
     duration: "33 min",
     thumb: "https://i.ytimg.com/vi/2alg7MQ6_sI/hqdefault.jpg",
     video_id: "2alg7MQ6_sI"
